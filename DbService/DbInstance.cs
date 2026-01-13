c2e1affc6c543669cc37747b04075dd45805f7e4
n-saraseka <nicksaraseka43@gmail.com>
2026-01-13 14:29:25 +0300

    Add basic DB service

---
+++ DbService/DbInstance.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using osuscorefetcher.ConfigHandler;
+using Npgsql;
+
+namespace osuscorefetcher.DbService
+{
+    public class DbInstance
+    {
+        private static readonly Config Config = ConfigIO.GetConfig();
+        private static readonly string ConnectionString = $"Host={Config.DbHost};Username={Config.DbUsername};Password={Config.DbPassword};Database={Config.DbName}";
+        public NpgsqlConnection Connection { get; private set; }
+        public DbInstance()
+        {
+            Connection = new NpgsqlConnection(ConnectionString);
+        }
+
+    }
+}
