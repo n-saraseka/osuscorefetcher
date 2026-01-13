e0af8f57c13cb0c42c192fb3dba3162b52161372
n-saraseka <nicksaraseka43@gmail.com>
2026-01-13 14:17:34 +0300

    Added DB related config fields

--- ConfigHandler/Config.cs
+++ ConfigHandler/Config.cs
@@ -1,10 +1,26 @@
-﻿namespace osuscorefetcher.ConfigHandler
+﻿using Newtonsoft.Json;
+
+namespace osuscorefetcher.ConfigHandler
 {
     internal class Config
     {
+        [JsonProperty("api_id")]
         public int ApiId { get; set; }
+        [JsonProperty("api_secret")]
         public string ApiSecret { get; set; }
+        [JsonProperty("access_token")]
         public string AccessToken { get; set; }
+        [JsonProperty("cursor_string")]
+        public string Cursor { get; set; }
+        [JsonProperty("expires_in")]
         public long ExpiresIn { get; set; }
+        [JsonProperty("db_host")]
+        public string DbHost { get; set; }
+        [JsonProperty("db_username")]
+        public string DbUsername { get; set; }
+        [JsonProperty("db_password")]
+        public string DbPassword { get; set; }
+        [JsonProperty("db_name")]
+        public string DbName { get; set; }
     }
 }
--- ConfigHandler/ConfigIO.cs
+++ ConfigHandler/ConfigIO.cs
@@ -5,11 +5,22 @@ namespace osuscorefetcher.ConfigHandler
     internal class ConfigIO
     {
         private static string ConfigPath = Path.Combine(Directory.GetCurrentDirectory(), "appconfig.json");
+
+        /// <summary>
+        /// Get latest Config object from ConfigPath
+        /// </summary>
+        /// <returns>Populated Config object</returns>
         public static Config GetConfig()
         {
             Config configFromFile = JsonConvert.DeserializeObject<Config>(File.ReadAllText(ConfigPath), new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
             return configFromFile;
         }
+        
+        /// <summary>
+        /// Save new config data from JSON string
+        /// </summary>
+        /// <param name="jsonData">JSON config data</param>
+        /// <returns>Updated Config object</returns>
         public static Config SetConfig(string jsonData) {
             File.WriteAllText(ConfigPath, jsonData);
             return GetConfig();
