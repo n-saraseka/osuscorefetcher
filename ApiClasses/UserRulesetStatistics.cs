7ff2bd6f431e674d2baa58fdf46a51fe82fffb4e
n-saraseka <nicksaraseka43@gmail.com>
2026-01-13 14:27:35 +0300

    Replace Rank with UserRulesetStatistics

---
+++ ApiClasses/UserRulesetStatistics.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace osuscorefetcher.ApiClasses
+{
+    internal class UserRulesetStatistics
+    {
+        [JsonProperty("global_rank")]
+        public int? GlobalRank { get; set; }
+        [JsonProperty("pp")]
+        public int PP { get; set; } = 0;
+    }
+}
