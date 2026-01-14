b0f854f3b24f4c95e63ab478c2e5217310d3916a
n-saraseka <nicksaraseka43@gmail.com>
2026-01-14 12:49:02 +0300

    add CountryCode field to User

--- ApiClasses/User.cs
+++ ApiClasses/User.cs
@@ -8,6 +8,8 @@ namespace osuscorefetcher.ApiClasses
         public int Id { get; set; }
         [JsonProperty("username")]
         public string Username { get; set; }
+        [JsonProperty("country_code")]
+        public string CountryCode { get; set; }
         [JsonProperty("country")]
         public Country Country { get; set; }
         [JsonProperty("statistics_rulesets")]
